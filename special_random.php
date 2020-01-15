9d5c9698cb02b9062dbf3c5f15368c48f753c3b4
badonkiedew <54218190+badonkiedew@users.noreply.github.com>
2020-01-15 15:44:00 -0500

    Update special_random.php

--- special_random.php
+++ special_random.php
@@ -1,8 +1,38 @@
+<html>
+
+<head>
+
+<style>
+.preview {
+    width:600px; height:400px;
+    -webkit-transform:scale(.25);
+    -ms-transform:scale(.5);
+    transform:scale(.5);
+    -webkit-transform-origin:0 0; 
+    -ms-transform-origin:0 0; 
+    transform-origin:0 0; 
+    border:4px solid blue;
+    margin:0 0 -300px 0;
+}
+</style>
+
+
+
+
+
 <?php
 //print "test";
 //echo phpinfo();
 /*
 curl -I https://en.wikipedia.org/wiki/Special:Random | grep location
 */
-echo exec('curl -I https://en.wikipedia.org/wiki/Special:Random | grep location');
+$wikiroll = exec('curl -I https://en.wikipedia.org/wiki/Special:Random | grep location');
+
+
+$wikiurl = trim($wikiroll,"location :");
+
+
+print "<iframe class=preview src=$wikiurl></iframe>";
 ?>
+
+
