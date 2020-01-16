3d10fa36308f8375f5ba81c52b3a66a5a68e26a1
badonkiedew <54218190+badonkiedew@users.noreply.github.com>
2020-01-15 21:18:22 -0500

    Create infinite-wiki.php

---
+++ infinite-wiki.php
@@ -0,0 +1,67 @@
+		<html>
+
+<head>
+
+</head>
+<style>
+.preview {
+    width: 100%;
+    height: 80%;
+    -webkit-transform:scale(.45);
+    -ms-transform:scale(.8);
+    transform:scale(.8);
+    -webkit-transform-origin:0 0; 
+    -ms-transform-origin:0 0; 
+    transform-origin:0 0; 
+    border:0px solid blue;
+    margin:auto;
+color: #9932cc;
+    box-shadow: 0px 4px 16px #b9ebff;
+    border: 1px solid #9932cc;
+    border-radius: 5px;
+	z-index: 0;
+}
+.visiturl {
+	z-index: 1;
+	}
+</style>
+
+
+				
+<body align=center bgcolor=#2a4b8d>
+
+
+	<?php
+	 
+	    
+
+	    
+	    
+	//print "test";
+	//echo phpinfo();
+
+	    $arr = array(1, 2, 3, 4, 5, 6, 7, 8);
+	foreach ($arr as &$columns) {
+	    $columns = $wikiurl;
+	$randomurl = exec('curl -I https://en.wikipedia.org/wiki/Special:Random | grep location');
+	    $wikiurl = trim($randomurl,"location :");
+	    print "<a class=visiturl href=$wikiurl><iframe scrolling=no align=middle class=preview src=$wikiurl></iframe>$wikiurl</a>";
+
+}
+	// $arr is now filled
+	    
+
+	    //unset($value); // break the reference with the last element
+
+
+
+
+
+
+
+	?>
+				</center>
+					
+
+
+
