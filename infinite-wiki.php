e964ea5f2eb365278942999f8ffef967892ad8ae
badonkiedew <54218190+badonkiedew@users.noreply.github.com>
2020-01-18 18:24:43 -0500

    Update infinite-wiki.php

--- infinite-wiki.php
+++ infinite-wiki.php
@@ -1,82 +1,40 @@
-		<html>
-
-<head>
-
-</head>
-<style>
-.preview {
-    width: 100%;
-    height: 80%;
-    -webkit-transform:scale(.45);
-    -ms-transform:scale(.8);
-    transform:scale(.8);
-    -webkit-transform-origin:0 0; 
-    -ms-transform-origin:0 0; 
-    transform-origin:0 0; 
-    border:0px solid blue;
-    margin:auto;
-color: #9932cc;
-    box-shadow: 0px 4px 16px #b9ebff;
-    border: 1px solid #9932cc;
-    border-radius: 5px;
-	z-index: 0;
-}
-.visiturl {
-	z-index: 1;
-	}
-</style>
-
-
-				
-<body align=center bgcolor=#2a4b8d>
-
-
-	<?php
+<?php
 	include("conn.php");
 
-		$timestamp = time();
-		print $timestamp; 
-	    
-
-	    
-	    
 	//print "test";
 	//echo phpinfo();
-
-	    $arr = array(1, 2, 3, 4, 5, 6, 7, 8);
+		
+	    $arr = array(1, 2, 3,);
 	foreach ($arr as &$columns) {
+		$timestamp = time();
+		print $timestamp; 
+		sleep(2);
 	    $columns = $wikiurl;
 	$randomurl = exec('curl -I https://en.wikipedia.org/wiki/Special:Random | grep location');
-	    $wikiurl = trim($randomurl,"location :");
+	    
+	$wikiurl = trim($randomurl,"location :");
+	
+
 	$urlin = mysqli_query($mysqli, "INSERT INTO urls VALUES('$timestamp','$wikiurl')");
-	$urlout = mysqli_query($mysqli, "SELECT wikinum, url FROM randomwiki where(wikinum ='$timestamp' && url ='$wikiurl')");
-		$timestamp = "";
-	if ($result = $mysqli->query($urlout)) {
-				
-			   
-	    while ($row = $result->mysqli_fetch_array()) {
 
-				$raw_num = $row[wikinum];
-				$raw_wiki = $row[url];
-		print $raw_num;
-		print $raw_wiki;
-			}
-			$result->close();
-				}
 		
-	    print "<a class=visiturl href=$wikiurl><iframe scrolling=no align=middle class=preview src=$wikiurl></iframe>$wikiurl</a>";
 
-}
-	// $arr is now filled
+
+
+	$urlout = mysqli_query($mysqli, "SELECT * FROM urls where(wikinum ='$timestamp')");
+		
+
+	
+
+
+	$select_row=mysqli_fetch_array($urlout);
+				$raw_num = $select_row["wikinum"];
+				$raw_wiki = $select_row["url"];
+		print "$raw_num";
+		print "$raw_wiki";
+
+print "<a class=visiturl href=$raw_wiki><iframe scrolling=no align=middle class=preview src=$raw_wiki></iframe>$raw_wiki</a>";
+		
 	    
 
-	    //unset($value); // break the reference with the last element
-
-
-
-
-
-
-
-	?>
-				</center>
+}
